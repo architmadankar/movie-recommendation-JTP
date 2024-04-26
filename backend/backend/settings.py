456b58d82a929cba386a8fcbcc10df136b21420f
Archit Madankar <architmadankar21@gmail.com>
2024-04-26 19:51:20 +0530

    fix urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'login',
+    'recommend',
+    'corsheaders'
+    
 ]
 
 
@@ -60,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
