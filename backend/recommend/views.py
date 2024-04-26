8f4bc2ad8e08853d373d8971d3eed0a33013a6c0
Archit Madankar <architmadankar21@gmail.com>
2024-04-27 00:45:58 +0530

    typo

--- backend/recommend/views.py
+++ backend/recommend/views.py
@@ -4,23 +4,26 @@ from rest_framework.permissions import IsAuthenticated
 from .models import * 
 from .serializer import *
 from engine import recommend
+from django.db.models import Q
 # Create your views here.
 
 class List(APIView):
     permission_classes = [IsAuthenticated]
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         movies = Recommend.objects.all()
         serializer = Serializer(movies, many = True)
         return Response(status = 200, data = serializer.data)
+    
 class Suggest(APIView):
-    classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         movie = request.data.get('movie', None)
         List = recommend(movie)
         res = []
         for i in List:
             if Recommend.objects.filter(id=i).exists():
-                mov = Recommend.objects.filter(id=i)[0]
-                serializedData = Serializer(mov)
+                x = Recommend.objects.filter(id=i)[0]
+                serializedData = Serializer(x)
                 res.append(serializedData.data)
-        return Response( status=200, data = List)
+        return Response( status=200, data = res)
+    
